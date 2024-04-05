1beccd3f84a187ce9dcb0288a7a218914e1a590a
NikolaBV <bulgarianmapper64@gmail.com>
2024-04-05 12:58:00 +0300

    react router implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
 import TopNav from "./components/TopNav"
+import Home from "./components/Home"
+import Blog from "./components/Blog"
+import UserDetails from "./components/UserDetails"
 
+import {Route, Routes } from "react-router-dom"
 function App() {
 
 
   return (
     <>
           <TopNav></TopNav>
+          <div className="container">
+              <Routes>
+                  <Route path="/" element={<Home></Home>}></Route>
+                  <Route path="/blog" element={<Blog></Blog>}></Route>
+                  <Route path="/userDetails" element={<UserDetails></UserDetails>}></Route>
+              </Routes>
+          </div>
     </>
   )
 }
